7f2e0f344f32d69d6d9f1042dbc319028f5f4ef5
tdrobbins <tylrdrbbns@gmail.com>
2021-06-04 19:28:38 -0700

    readded facts files

---
+++ facts_gen.py
@@ -0,0 +1,23 @@
+import yaml
+
+facts = []
+
+with open("./licenses.yml") as file:
+    lics = yaml.load(file)
+
+    for l in lics:
+        lic = lics[l]
+        facts.append("cat(\"{}\",\"{}\")\n".format(l, lic["cat"]))
+
+        if "addreq" in lic.keys():
+            for r in lic["addreq"]:
+                facts.append("addreq(\"{}\",\"{}\")\n".format(l, r))
+
+        if "comp" in lic.keys():
+            for c in lic["comp"]:
+                facts.append("comp(\"{}\",\"{}\")\n".format(l, c))
+        if "gc" in lic.keys():
+            facts.append("gc(\"{}\")\n".format(l))
+
+with open("facts.txt","w") as out:
+    out.writelines(facts)
